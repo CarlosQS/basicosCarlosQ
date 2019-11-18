7734bb333d64e8b5c5741fcf60eabd2902cbe0af
Carlos Quiles Serrano <carlos.qs.01.cqs@gmail.com>
2019-11-18 11:58:25 +0100

    Añadido clase Cadenas y su codigo

---
+++ src/basicosCarlosQ/Cadenas.java
@@ -0,0 +1,14 @@
+package basicosCarlosQ;
+
+public class Cadenas {
+
+	public static void cadenas() {
+		String x = "Hola, soy un String";
+		System.out.println(x);
+	}
+	
+	public static void main(String[] args) {
+		cadenas();
+	}
+	
+}
